197cecf74b306bce187b628ce9c3a27b67599bc5
João Graça <joao.g.n91@gmail.com>
2019-09-15 22:21:13 -0300

    Add Cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
 
@@ -20,6 +21,7 @@ class App {
 
   middlerware():void {
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')),
